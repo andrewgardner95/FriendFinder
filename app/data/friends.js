d0b7bf92ebb113f9f301b8c3b44e649ab1f7708e
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-04-22 08:35:24 -0700

    to do: survey javascript

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+// The below array will hold all of the profiles on FriendFinder
+var friendsArray = [
+  {
+    name: "Patrick Star",
+    photo: "http://carboncostume.com/wordpress/wp-content/uploads/2014/09/patrickstar.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  },{
+    name: "Harry Potter",
+    photo: "http://i4.mirror.co.uk/incoming/article3827727.ece/ALTERNATES/s615b/Harry-Potter-and-The-Chamber-of-Secrets.jpg",
+    scores: ["2", "2", "3", "4", "3", "1", "1", "1", "1", "3"]
+  }
+];
+
+// Exporting the array to make it accessible to other files using require.
+module.exports = friendsArray;
